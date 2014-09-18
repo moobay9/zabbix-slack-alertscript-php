44697a6740e5949df58bd35744d6d567fd6d0a77
Masanori Oobayashi <moobay9@gmail.com>
2014-09-18 15:01:07 +0900

    1st commit

---
+++ slack.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+$config = parse_ini_file('config.ini');
+
+$token   = $argv['1'];
+$subject = $argv['2'];
+$body    = $argv['3'];
+
+$postdata = array('payload' => json_encode(
+	array(
+		'username' => $config['username'],
+		'icon_url' => $config['icon'],
+		'text'     => $body,
+		'channel'  => $config['channel'],
+	))
+);
+
+$ch = curl_init($config['url'].'?'.http_build_query(array('token' => $token)));
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_exec($ch);
+curl_close($ch);
